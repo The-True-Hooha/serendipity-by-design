f2f8d16dc1140681baa0c2ff8155f651536e1ad3
David Ogar <owogogahhero@outlook.com>
2022-07-27 00:01:38 +0100

    refactor: bg color

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer(){
     return(
         <footer className='inset-x-0 bottom-0'>
-            <div className='lg:px-[300px] shadow-lg py-4 border'>
+            <div className='lg:px-[300px] shadow-lg py-4 border bg-gray-200/40'>
                 <p className='font-sora px-[30px]'>&copy; 2022 David Ogar</p>
             </div>
         </footer>
