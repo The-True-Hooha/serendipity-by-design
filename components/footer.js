6f9129fd5587053d771776df7aecae2ff4a0dddc
David Ogar <owogogahhero@outlook.com>
2023-01-20 12:36:04 +0100

    refactor: copyright year claim

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ export default function Footer(){
     return(
         <footer className='inset-x-0 bottom-0 '>
             <div className='lg:px-[300px] shadow-lg py-4 border bg-slate-300'>
-                <p className='font-sora font-bold px-[30px]'>&copy; 2022 David Ogar</p>
+                <p className='font-sora font-bold px-[30px]'>&copy; 2023 David Ogar</p>
             </div>
         </footer>
     )
